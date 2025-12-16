763e4763c3213e4fba9aac86b5d6c5f6d1b1bd70
Fannareme Aboubacar Abdou <125912257+Aboubacarabdoufannareme@users.noreply.github.com>
2025-12-17 06:07:50 +0900

    Update candidate_documents.php

--- candidate_documents.php
+++ candidate_documents.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // candidate_documents.php
 require_once 'includes/header.php';
 require_login();
