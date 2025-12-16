83044ee9baeaa8495582b8cb4f06d0f2a56ff1dc
Fannareme Aboubacar Abdou <125912257+Aboubacarabdoufannareme@users.noreply.github.com>
2025-12-17 07:52:08 +0900

    Update candidate_documents.php

--- candidate_documents.php
+++ candidate_documents.php
@@ -1,4 +1,8 @@
 <?php
+// Turn on full error reporting for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // candidate_documents.php - DATABASE STORAGE VERSION
 require_once 'includes/header.php';
 require_login();
